08abf9c75e0ed80c2795d1f60f13256d725d02f4
Ahmed El Gabri <ahmed@gabri.me>
2017-01-25 22:11:35 +0100

    add 404 title

--- pages/_error.js
+++ pages/_error.js
@@ -1,5 +1,6 @@
 // @flow
 import React from 'react'
+import Head from 'next/head'
 import Back from '../components/Back'
 
 export default class Error extends React.Component {
@@ -11,8 +12,11 @@ export default class Error extends React.Component {
   render () {
     return (
       <div>
+        <Head>
+          <title>Oops - page not found 🙈</title>
+        </Head>
         <h2>ERROR {this.props.errorCode} - PAGE NOT FOUND</h2>
-        <img src="https://media.giphy.com/media/uZvpSc5LVa3hS/giphy.gif" style={{maxWidth: '100%'}} />
+        <img src='/static/img/404.gif' style={{maxWidth: '100%'}} />
         <Back />
       </div>
     )
