a378baf83a9bc76065377eb88c29d86555ef358e
Ahmed El Gabri <ahmed@gabri.me>
2017-01-25 22:49:41 +0100

    fix redirect

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app
     server.get(/\/feed((\/)|(\.xml))?/, (req, res) => res.redirect(301, '/feed.xml'))
     // server.get(/\/sitemap((\/)|(\.xml))?/, (req, res) => res.redirect(301, '/sitemap.xml'))
     server.get(/\/(work(\/(.+)?)|blog)(\/)?$/, (req, res) => res.redirect(301, '/'))
-    server.get(/\/blog(\/)?/, (req, res) => res.redirect(301, '/'))
+    server.get('/blog(\/)?', (req, res) => res.redirect(301, '/'))
 
     server.get('/blog/:post', (req, res) => app.render(req, res, '/blog', req.params))
     server.get('*', (req, res) => handle(req, res))
