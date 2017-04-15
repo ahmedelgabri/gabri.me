55b653b030e2b45c58431336c5278c51c55b260b
Ahmed El Gabri <ahmed@gabri.me>
2017-04-15 21:47:37 +0200

    add event & page visibility trackers

--- components/Contact.js
+++ components/Contact.js
@@ -6,23 +6,33 @@ import data from '../data.json'
 const s = {
   list: cxs({
     margin: 0,
-    padding: 0
+    padding: 0,
   }),
   item: cxs({
     display: 'inline-block',
-    marginRight: '1rem'
-  })
+    marginRight: '1rem',
+  }),
 }
 
-export default ({ social = data.social }) =>
-  <div role='contentinfo'>
+export default ({ social = data.social }) => (
+  <div role="contentinfo">
     <ul className={s.list}>
       {Object.keys(social).map(site => {
         return (
           <li key={site} className={s.item}>
-            <a href={social[site]} rel='me' target='_blank'>{site}</a>
+            <a
+              href={social[site]}
+              rel="me"
+              target="_blank"
+              data-ga-on="click"
+              data-ga-event-category="Social links"
+              data-ga-event-action={social[site]}
+            >
+              {site}
+            </a>
           </li>
         )
       })}
     </ul>
   </div>
+)
