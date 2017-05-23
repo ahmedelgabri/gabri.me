9c19ce633d491d068b0db3bf8d269720a612c51e
Ahmed El Gabri <ahmed@gabri.me>
2017-05-23 23:04:49 +0200

    Writing => Blog

--- components/PostList.js
+++ components/PostList.js
@@ -6,20 +6,20 @@ import cxs from 'cxs'
 import metadata from '../posts/articles/metadata.json'
 
 const s = cxs({
-  lineHeight: 1.5
+  lineHeight: 1.5,
 })
 
 export default () => (
   <div className="mb1">
-    <h3>📝 Writing</h3>
+    <h3>📝 Blog</h3>
     <ul className={s}>
-      {
-        Object.keys(metadata).map(post =>
-          <li key={metadata[post].attributes.title}>
-            <Link prefetch href={`/blog?post=${post}`} as={`/blog/${post}`}><a>{metadata[post].attributes.title}</a></Link>
-          </li>
-        )
-      }
+      {Object.keys(metadata).map(post => (
+        <li key={metadata[post].attributes.title}>
+          <Link prefetch href={`/blog?post=${post}`} as={`/blog/${post}`}>
+            <a>{metadata[post].attributes.title}</a>
+          </Link>
+        </li>
+      ))}
     </ul>
   </div>
 )
