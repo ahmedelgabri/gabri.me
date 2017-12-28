949900b836c09abe32078922c3b3d34bd00dd8cb
Ahmed El Gabri <ahmed@gabri.me>
2017-12-28 17:49:36 +0100

    http://www.phpied.com/minimum-viable-sharing-meta-tags/

--- src/components/Meta/index.js
+++ src/components/Meta/index.js
@@ -1,35 +1,64 @@
 // @flow
-import React from 'react'
+import * as React from 'react'
 import Helmet from 'react-helmet'
+import config from '../../../gatsby-config'
+import msTile from '../../../static/img/ms-logo.png'
+import appleIcon57 from '../../../static/apple-touch-icon-57x57-precomposed.png'
+import appleIcon72 from '../../../static/apple-touch-icon-72x72-precomposed.png'
+import appleIcon114 from '../../../static/apple-touch-icon-114x114-precomposed.png'
+
+const { description, social, twitterId, siteUrl } = config.siteMetadata
+const socialImg = `${siteUrl}/img/fb-image.jpg`
 
 export default ({
   title,
   url,
-  excerpt = title,
+  excerpt = description,
   post,
-  img = '/img/fb-image.png',
+  img = socialImg,
   children,
 }) => {
   return (
     <Helmet>
       <title>{title}</title>
-
-      {/* G+ */}
-      <meta itemProp="name" content={title} />
-      <meta itemProp="description" content={excerpt} />
-      <meta itemProp="image" content={img} />
-
-      {/* FB Open Graph meta tags */}
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={excerpt} />
+      <meta name="description" content={excerpt} />
       <meta property="og:url" content={url} />
       <meta property="og:image" content={img} />
       <meta property="og:type" content={post ? 'article' : 'website'} />
-
-      {/* twitter */}
-      <meta name="twitter:url" content={url} />
       <meta name="twitter:title" content={title} />
-      <meta name="twitter:image" content="/img/logo.svg" />
+      <meta name="twitter:description" content={excerpt} />
+
+      <link
+        type="text/plain"
+        rel="author"
+        href="https://plus.google.com/101787568188227600845/"
+      />
+      <meta name="application-name" content="Gabri.me" />
+      <link name="msapplication-TileImage" href={msTile} />
+      <link name="msapplication-TileColor" content="#1f2325" />
+      <meta name="theme-color" content="#1f2325" />
+
+      <link
+        rel="apple-touch-icon-precomposed"
+        sizes="114x114"
+        href={appleIcon114}
+      />
+      <link
+        rel="apple-touch-icon-precomposed"
+        sizes="72x72"
+        href={appleIcon72}
+      />
+      <link rel="apple-touch-icon-precomposed" href={appleIcon57} />
+      <meta name="apple-mobile-web-app-title" content="Gabri.me" />
+      <link
+        rel="author"
+        href="https://plus.google.com/101787568188227600845/posts"
+      />
+      <meta property="twitter:account_id" content={twitter_id} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content={social.twitter.url} />
+      <meta name="twitter:domain" content={siteUrl} />
+
       {children}
     </Helmet>
   )
--- src/html.js
+++ src/html.js
@@ -1,14 +1,8 @@
 // @flow
-import React from 'react'
+import * as React from 'react'
 import Helmet from 'react-helmet'
 import { minify } from 'csso'
-import config from '../gatsby-config'
 import favicon from '../static/favicon.png'
-import msTile from '../static/img/ms-logo.png'
-import logo from '../static/img/logo.svg'
-import appleIcon57 from '../static/apple-touch-icon-57x57-precomposed.png'
-import appleIcon72 from '../static/apple-touch-icon-72x72-precomposed.png'
-import appleIcon114 from '../static/apple-touch-icon-114x114-precomposed.png'
 import { globalStyles } from './utils/style'
 
 let stylesStr = ''
@@ -27,52 +21,17 @@ const cssStr =
 
 export default function HTML(props) {
   const head = Helmet.rewind()
-  const { description, social, twitter_id, siteUrl } = config.siteMetadata
 
   return (
     <html lang="en">
       <head>
         <meta chartset="utf-8" />
-        <meta name="description" content={description} />
         <meta name="viewport" content="initial-scale=1, width=device-width" />
         <style
           id="gatsby-inlined-css"
           dangerouslySetInnerHTML={{ __html: cssStr }}
         />
         <link rel="shortcut icon" href={favicon} />
-        <link
-          type="text/plain"
-          rel="author"
-          href="https://plus.google.com/101787568188227600845/"
-        />
-        <meta name="application-name" content="Gabri.me" />
-        <link name="msapplication-TileImage" href={msTile} />
-        <link name="msapplication-TileColor" content="#1f2325" />
-        <meta name="theme-color" content="#1f2325" />
-
-        <link
-          rel="apple-touch-icon-precomposed"
-          sizes="114x114"
-          href={appleIcon114}
-        />
-        <link
-          rel="apple-touch-icon-precomposed"
-          sizes="72x72"
-          href={appleIcon72}
-        />
-        <link rel="apple-touch-icon-precomposed" href={appleIcon57} />
-        <link rel="logo" type="image/svg" href={logo} />
-        <meta name="apple-mobile-web-app-title" content="Gabri.me" />
-        <link
-          rel="author"
-          href="https://plus.google.com/101787568188227600845/posts"
-        />
-
-        <meta property="twitter:account_id" content={twitter_id} />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:creator" content={social.twitter.url} />
-        <meta name="twitter:domain" content={siteUrl} />
-
         {props.headComponents}
       </head>
       <body>
