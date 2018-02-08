ac414a0f2d504d6f3444997ece80f471fc145744
Ahmed El Gabri <ahmed@gabri.me>
2018-02-08 22:05:50 +0100

    Get rid of the hack to clean up the service worker

--- src/html.js
+++ src/html.js
@@ -27,35 +27,6 @@ export default function HTML(props) {
       <body>
         <div id="___gatsby" dangerouslySetInnerHTML={{ __html: props.body }} />
         {props.postBodyComponents}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              // Get rid of the annoying service worker
-              if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
-                window.navigator.serviceWorker
-                  .getRegistrations()
-                  .then(function(registrations) {
-                    var sws = registrations.filter(function(r) {
-                      r.scope.includes(window.location.origin) &&
-                        r.active &&
-                        r.active.state &&
-                        r.active.state === 'activated'
-                    })
-
-                    sws.forEach(function(r) {
-                      r.active.state && r.active.state !== 'redundant' ? r.unregister() : null
-                    })
-
-                    return sws
-                  })
-                  .then(function(sws) {
-                    sws.length && window.location.reload()
-                  })
-                  .catch(err => window.console && console.log('I\'m very sorry! can you try to refresh the page, unregistering a service worker is annoying!'))
-              }
-            `,
-          }}
-        />
       </body>
     </html>
   )
