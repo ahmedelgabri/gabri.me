63a2f1e0aa8fb56fdb72f689ff6a04c0b15ce105
Ahmed El Gabri <ahmed@gabri.me>
2019-01-12 16:06:35 +0100

    Update emotion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import {StaticQuery, graphql} from 'gatsby'
-import {injectGlobal} from 'emotion'
+import {Global, css} from '@emotion/core'
 import Meta from '../components/Meta'
 import Layout from '../components/Layout'
 import Logo from '../components/Logo'
@@ -11,8 +11,6 @@ import Contact from '../components/Contact'
 import Footer from '../components/Footer'
 import {globalStyles} from '../utils/style'
 
-injectGlobal`${globalStyles}`
-
 interface Data {
   site: SiteMetaT
   allMdx: {
@@ -22,118 +20,125 @@ interface Data {
 
 export default function Index() {
   return (
-    <StaticQuery
-      query={graphql`
-        query indexQuery {
-          site {
-            siteMetadata {
-              author
-              title
-              siteUrl
-              description
-              social {
-                twitter {
-                  display
-                  url
+    <>
+      <Global
+        styles={css`
+          ${globalStyles}
+        `}
+      />
+      <StaticQuery
+        query={graphql`
+          query indexQuery {
+            site {
+              siteMetadata {
+                author
+                title
+                siteUrl
+                description
+                social {
+                  twitter {
+                    display
+                    url
+                  }
+                  github {
+                    display
+                    url
+                  }
+                  email {
+                    display
+                    url
+                  }
+                  linkedin {
+                    display
+                    url
+                  }
+                  resume {
+                    display
+                    url
+                  }
                 }
-                github {
-                  display
-                  url
+                talks {
+                  AmsterdamJS
                 }
-                email {
-                  display
-                  url
-                }
-                linkedin {
-                  display
-                  url
-                }
-                resume {
-                  display
-                  url
-                }
-              }
-              talks {
-                AmsterdamJS
-              }
-              interviews {
-                devtomanager
-              }
-            }
-          }
-          allMdx(
-            sort: {fields: [frontmatter___date], order: DESC}
-            filter: {fileAbsolutePath: {regex: "/articles/"}}
-          ) {
-            edges {
-              node {
-                fields {
-                  slug
-                }
-                frontmatter {
-                  title
+                interviews {
+                  devtomanager
                 }
               }
             }
+            allMdx(
+              sort: {fields: [frontmatter___date], order: DESC}
+              filter: {fileAbsolutePath: {regex: "/articles/"}}
+            ) {
+              edges {
+                node {
+                  fields {
+                    slug
+                  }
+                  frontmatter {
+                    title
+                  }
+                }
+              }
+            }
           }
-        }
-      `}
-      render={(data: Data) => {
-        const {
-          author,
-          social,
-          talks,
-          interviews,
-          title,
-          siteUrl,
-        } = data.site.siteMetadata
-        const posts = data.allMdx.edges
+        `}
+        render={(data: Data) => {
+          const {
+            author,
+            social,
+            talks,
+            interviews,
+            title,
+            siteUrl,
+          } = data.site.siteMetadata
+          const posts = data.allMdx.edges
 
-        return (
-          <Layout>
-            <div css={{paddingBottom: '1rem', marginBottom: '1rem'}}>
-              <Meta title={`${author} | ${title}`} url={siteUrl} />
-              <Logo />
-              <div css={{paddingBottom: '1rem', paddingTop: '1rem'}}>
-                <h1 css={{fontWeight: 500}}>{author}</h1>
-                <p>
-                  Principal Software Developer at{' '}
-                  <a target="_blank" href="http://lightspeedhq.com">
-                    LightspeedHQ
-                  </a>
-                  <br />
-                  Based in Amsterdam, The Netherlands.
-                </p>
+          return (
+            <Layout>
+              <div css={{paddingBottom: '1rem', marginBottom: '1rem'}}>
+                <Meta title={`${author} | ${title}`} url={siteUrl} />
+                <Logo />
+                <div css={{paddingBottom: '1rem', paddingTop: '1rem'}}>
+                  <h1 css={{fontWeight: 500}}>{author}</h1>
+                  <p>
+                    Principal Software Developer at{' '}
+                    <a target="_blank" href="http://lightspeedhq.com">
+                      LightspeedHQ
+                    </a>
+                    <br />
+                    Based in Amsterdam, The Netherlands.
+                  </p>
+                </div>
+                <Contact social={social} />
               </div>
-              <Contact social={social} />
-            </div>
-            <div
-              css={{
-                '@media screen and (min-width: 48em)': {
-                  display: 'flex',
-                },
-
-                '> div': {
+              <div
+                css={{
                   '@media screen and (min-width: 48em)': {
-                    marginRight: '4rem',
+                    display: 'flex',
                   },
 
-                  flexBasis: '50%',
-                },
-              }}
-            >
-              <div>
-                <PostList posts={posts} />
+                  '> div': {
+                    '@media screen and (min-width: 48em)': {
+                      marginRight: '4rem',
+                    },
+
+                    flexBasis: '50%',
+                  },
+                }}
+              >
+                <div>
+                  <PostList posts={posts} />
+                </div>
+                <div>
+                  <Talks talks={talks} />
+                  <Interviews interviews={interviews} />
+                </div>
               </div>
-              <div>
-                <Talks talks={talks} />
-                <Interviews interviews={interviews} />
-              </div>
-            </div>
-            <Footer author={author} />
-          </Layout>
-        )
-      }}
-    />
+              <Footer author={author} />
+            </Layout>
+          )
+        }}
+      />
+    </>
   )
 }
