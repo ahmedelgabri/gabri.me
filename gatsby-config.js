e37a1cf25af93d50f795d0de3e78073def765eb2
Ahmed El Gabri <ahmed@gabri.me>
2019-05-10 11:14:37 +0200

    Split config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 const proxy = require('http-proxy-middleware')
+const siteMetadata = require('./src/config/meta')
 const feedUrl = 'feed.xml'
 
 module.exports = {
@@ -13,47 +14,7 @@ module.exports = {
       }),
     )
   },
-  siteMetadata: {
-    author: 'Ahmed El Gabri',
-    title: 'Front-end Engineer',
-    siteUrl: 'https://gabri.me',
-    description:
-      'Ahmed El Gabri is a Front-end Engineer who like to bring structure where it is lacking, systematizing information & automating processes.',
-    social: {
-      twitter: {
-        display: '@ahmedelgabri',
-        url: 'http://twitter.com/ahmedelgabri',
-      },
-      github: {
-        display: 'github.com/ahmedelgabri',
-        url: 'https://github.com/ahmedelgabri',
-      },
-      email: {
-        display: 'email',
-        url: 'mailto:ahmed+contact@gabri.me?subject=Hi!&body=👋🏼',
-      },
-      linkedin: {
-        display: 'ahmedelgabri',
-        url: 'https://www.linkedin.com/in/ahmedelgabri/',
-      },
-      resume: {
-        display: 'resume',
-        url:
-          'https://docs.google.com/document/d/1sxu8gQi_vyz_RnDNTb6qJy3o9cth6_DzAI2zE_HHSnQ/edit?usp=sharing',
-      },
-    },
-    disqus: 'gabrime',
-    twitterId: '1512909779',
-    talks: {
-      AmsterdamJS: ['http://bit.ly/amsterdamjs-codemods', 'xGjSMbks9vA'],
-    },
-    interviews: {
-      devtomanager: [
-        'Developer to manager blog interview',
-        'https://devtomanager.com/interviews/ahmed-el-gabri/',
-      ],
-    },
-  },
+  siteMetadata,
   plugins: [
     'gatsby-plugin-remove-serviceworker',
     'gatsby-plugin-twitter',
