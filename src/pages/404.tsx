03710054b78c1e1a790ae480f5e13e16f19c31af
Ahmed El Gabri <ahmed@gabri.me>
2020-01-27 21:23:18 +0100

    Use hooks for querying

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,38 +1,35 @@
 import * as React from 'react'
 import Helmet from 'react-helmet'
-import {StaticQuery, graphql} from 'gatsby'
+import {useStaticQuery, graphql} from 'gatsby'
 import Layout from '../components/Layout'
 import Back from '../components/Back'
 import Footer from '../components/Footer'
 import gif from '../../static/img/404.gif'
 
-interface Data {
-	site: {siteMetadata: {author: string}}
-}
-
 export default function Error() {
-	return (
-		<StaticQuery
-			query={graphql`
-				query errorQuery {
-					site {
-						siteMetadata {
-							author
-						}
-					}
+	const {
+		site: {
+			siteMetadata: {author},
+		},
+	} = useStaticQuery(graphql`
+		query errorQuery {
+			site {
+				siteMetadata {
+					author
 				}
-			`}
-			render={(data: Data) => (
-				<Layout>
-					<Helmet>
-						<title>Oops - page not found 🙈</title>
-					</Helmet>
-					<Back />
-					<h2>Sorry! 😰, this page was not found.</h2>
-					<img src={gif} alt="" style={{maxWidth: '100%'}} />
-					<Footer author={data.site.siteMetadata.author} />
-				</Layout>
-			)}
-		/>
+			}
+		}
+	`)
+
+	return (
+		<Layout>
+			<Helmet>
+				<title>Oops - page not found 🙈</title>
+			</Helmet>
+			<Back />
+			<h2>Sorry! 😰, this page was not found.</h2>
+			<img src={gif} alt="" style={{maxWidth: '100%'}} />
+			<Footer author={author} />
+		</Layout>
 	)
 }
