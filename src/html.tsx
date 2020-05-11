cfd16d271a696ed570fb44d03def02e5c686bab3
Ahmed El Gabri <ahmed@gabri.me>
2020-05-11 22:39:04 +0200

    Update packages

--- src/html.tsx
+++ src/html.tsx
@@ -22,10 +22,6 @@ export default class HTML extends React.Component<
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap"
-            rel="stylesheet"
-          />
           <link rel="shortcut icon" href={favicon} />
           {this.props.headComponents}
         </head>
