0e4898944625a817efae3eb748f52efc7291e683
Ahmed El Gabri <ahmed@gabri.me>
2020-10-27 23:19:18 +0100

    Update packages

--- pages/api/card.ts
+++ pages/api/card.ts
@@ -1,4 +1,4 @@
-import {NowRequest, NowResponse} from '@now/node'
+import {NowRequest, NowResponse} from '@vercel/node'
 import {getCard} from 'ahmedelgabri'
 
 const curl_user_agent_pattern = /^curl\/\d+\.\d+\.\d+$/
