549d01f14e9de73207b2b28963f36713beadfeb4
Ahmed El Gabri <ahmed@gabri.me>
2021-08-14 20:07:18 +0200

    Update Next.js 11 -> 11.1

---
+++ global.d.ts
@@ -0,0 +1,70 @@
+// This file is used to hold ambient type declarations, as well as type shims
+// for npm module without type declarations, and assets files.
+
+// For example, to shim modules without declarations, use:
+// declare module "package-without-declarations"
+
+// And to shim assets, use (one file extension per `declare`):
+// declare module "*.png"
+
+declare module 'unified'
+declare module 'remark-parse'
+declare module 'remark-rehype'
+declare module 'rehype-raw'
+declare module 'rehype-sanitize'
+declare module 'rehype-stringify'
+declare module 'mdx-prism'
+declare module 'ahmedelgabri' {
+  export function getCard(): string
+}
+
+declare module '*.png'
+declare module '*.svg'
+declare module '*.gif'
+declare module '*.jpg'
+
+declare type Theme = 'dark' | 'light'
+
+interface Window {
+  gtag: any
+  twttr: any
+  __onThemeChange(theme?: Theme): void
+  __theme: Theme
+  __setPreferredTheme(theme: Theme): void
+}
+
+// Global/Shared Props
+interface PostT {
+  title: string
+  slug: string
+  date: string
+}
+
+enum ContactT {
+  'twitter',
+  'github',
+  'email',
+  'linkedin',
+  'resume',
+}
+
+interface ActivityT {
+  [key: string]: string[]
+}
+
+interface SocialT {
+  [key in ContactT]: {
+    display: string
+    url: string
+  }
+}
+
+interface SiteMetaT {
+  author: string
+  title: string
+  siteUrl: string
+  description: string
+  social: SocialT
+  talks: Activity
+  interviews: Activity
+}
