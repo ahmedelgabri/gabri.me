387e1cbdbe9bc593315316e0bb5a221732d46d03
Ahmed El Gabri <ahmed@gabri.me>
2021-11-28 01:42:55 +0100

    don't limit /api/card to curl

--- src/pages/api/card.ts
+++ src/pages/api/card.ts
@@ -1,17 +1,6 @@
-import {NowRequest, NowResponse} from '@vercel/node'
+import {VercelResponse, VercelRequest} from '@vercel/node'
 import {getCard} from 'ahmedelgabri'
 
-const curl_user_agent_pattern = /^curl\/\d+\.\d+\.\d+$/
-
-export default (req: NowRequest, res: NowResponse) => {
-  const agent = req.headers['user-agent']
-  const agent_is_curl = curl_user_agent_pattern.test(agent)
-
-  if (agent_is_curl) {
-    res.send(getCard())
-  } else {
-    res.status(400).json({
-      message: `Try curl -L ${req.headers.host}${req.url} instead`,
-    })
-  }
+export default function (_: VercelRequest, res: VercelResponse) {
+  res.send(getCard())
 }
