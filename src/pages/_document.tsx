b00602477c9b98943d9aa93786389a5fe6dc1125
Ahmed El Gabri <ahmed@gabri.me>
2023-03-02 23:10:28 +0100

    Update packages

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,7 +15,7 @@ export default class MyDocument extends Document {
 					<link
 						rel="preconnect"
 						href="https://fonts.gstatic.com"
-						crossOrigin="true"
+						crossOrigin="anonymous"
 					/>
 					<link
 						href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;1,400;1,700&display=swap"
